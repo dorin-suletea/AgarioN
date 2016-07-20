c34f2c5b24069b5b00ab32ee9ebaf333b89f66da
dorinsuletea <dsuletea@metabiota.com>
2016-07-20 18:21:52 +0300

    Clean

--- src/com/next/model/World.java
+++ src/com/next/model/World.java
@@ -1,13 +1,4 @@
 package com.next.model;
-/*************************************************************************
- * Copyright (c) Metabiota Incorporated - All Rights Reserved
- * ------------------------------------------------------------------------
- * This material is proprietary to Metabiota Incorporated. The
- * intellectual and technical concepts contained herein are proprietary
- * to Metabiota Incorporated. Reproduction or distribution of this
- * material, in whole or in part, is strictly forbidden unless prior
- * written permission is obtained from Metabiota Incorporated.
- *************************************************************************/
 
 import com.next.ai.GeneticAlgorithm;
 
